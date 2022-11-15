ade60e8a10fe96a3b29a72a5989a38510f65672f
Bertrand LE GAL <blegal@users.noreply.github.com>
2022-11-15 18:09:00 +0100

    Update galaxeirb.cu

--- src/kernel/cuda/galaxeirb.cu
+++ src/kernel/cuda/galaxeirb.cu
@@ -50,13 +50,7 @@
         const float4* v_dat,
         const int   nElements)
 {
-    int particule_id = blockDim.x * blockIdx.x + threadIdx.x;
-    
-    if( particule_id < nElements )
-    {
-        const float4 newPos   = n_pos[ particule_id ];
-        c_pos[ particule_id ] = newPos;
-    }
+
 }
 /*
  *
@@ -71,87 +65,7 @@ __global__ void body2Body_exec(
               float4* v_dat,
         const int     nElements)
 {
-
-    extern __shared__ float4 shPosition[THREADGROUP_SIZE];
-
-    const int tid          = threadIdx.x;
-    const int particule_id = blockIdx.x * blockDim.x + threadIdx.x;
-
-    //
-    // Si jamais a cause des threads/grilles ont deborde...
-    //
-
-    if (particule_id < nElements)
-    {
-        //
-        // Dans le cas contraire on commence notre calcul !
-        //
-
-        float4 myPosition = c_pos[ particule_id ];
-        float4 myVelocity = v_dat[ particule_id ];
-
-        //
-        //
-        //
-
-        for (int idx = 0; idx < nElements; idx += blockDim.x)
-        {
-
-            shPosition[tid] = c_pos[idx + tid];
-
-            __syncthreads();
-
-            #pragma unroll 4
-            for (int p = 0; p < blockDim.x; p += 1)
-            {
-
-                const float4 other = shPosition[p];
-                float3 r;
-                r.x = other.x - myPosition.x;
-                r.y = other.y - myPosition.y;
-                r.z = other.z - myPosition.z;
-
-                float d = (r.x * r.x) + (r.y * r.y) + (r.z * r.z);
-                
-                float d3;
-                if ( d < 1.0f )
-                {
-                    d3 = 10.0f * other.w; // Multi modifiable
-                }
-                else
-                {
-                    d  = sqrt(d);
-                    d3 = 10.0f * other.w / (d * d * d); // Multi modifiable
-                }
-
-                myVelocity.x += r.x * d3;
-                myVelocity.y += r.y * d3;
-                myVelocity.z += r.z * d3;
-            }
-
-            __syncthreads();
-
-        }
-
-        //
-        // update position
-        //
-
-        __syncthreads();
-
-        myPosition.x = myPosition.x + (myVelocity.x * 0.01f);
-        myPosition.y = myPosition.y + (myVelocity.y * 0.01f);
-        myPosition.z = myPosition.z + (myVelocity.z * 0.01f);
-
-        //
-        // update device memory
-        //
-
-        n_pos[ particule_id ] = myPosition;     // NEXT POSITION !
-        v_dat[ particule_id ] = myVelocity;     // NEW  VELOCITY !
-    }
-
-    __syncthreads();
+ 
 }
 
 #endif
